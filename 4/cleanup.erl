086072eb7b80489a9115ca2452ae6c5d5ef3d205
rpienaar <ruan.pienaar@superbet.com>
2022-12-04 14:56:38 +0000

    Day 4 - part1 complete

---
+++ 4/cleanup.erl
@@ -0,0 +1,35 @@
+-module(cleanup).
+
+-export([run/0]).
+
+run() ->
+    {ok, FPID} = file:open("input.txt", [read, read_ahead, binary]),
+    check_elf_pair_assignments(FPID, file:read_line(FPID)).
+
+check_elf_pair_assignments(_FPID, eof) ->
+    0;
+check_elf_pair_assignments(FPID, {ok, Line}) ->
+    check_if_elf_pair_overlap(split_and_transform_line_input(Line)) +
+    check_elf_pair_assignments(FPID, file:read_line(FPID)).
+
+split_and_transform_line_input(Line) ->
+    lists:map(
+        fun(Str) -> binary_to_integer(Str) end,
+        binary:split(Line, input_delimiters(), [global, trim_all])
+    ).
+
+input_delimiters() ->
+    [<<"-">>, <<",">>, <<"\n">>].
+
+% Elf1 check
+check_if_elf_pair_overlap([Elf1Start, Elf1End, Elf2Start, Elf2End])
+        when Elf1Start =< Elf2Start andalso
+             Elf1End >= Elf2End ->
+    1;
+%% Elf2 check
+check_if_elf_pair_overlap([Elf1Start, Elf1End, Elf2Start, Elf2End])
+        when Elf2Start =< Elf1Start andalso
+             Elf2End >= Elf1End ->
+    1;
+check_if_elf_pair_overlap(_) ->
+    0.
